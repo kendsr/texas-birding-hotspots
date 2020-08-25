fb21c3b5ececb786f6e59d40f95505a05dcb9110
kendsr <kndulin@comcast.net>
2020-08-25 09:33:27 -0500

    support migration to mongodb/atlas

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/Birding';
+    process.env.DB_URI = 'mongodb://localhost:27017/Birding';
 } 
