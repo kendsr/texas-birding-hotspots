257d6897ee7909a828fb598f51e275d384cf1494
kendsr <kndulin@comcast.net>
2020-08-28 12:11:42 -0500

    Change connection string to support dev database on mongodb atlas

--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.DB_URI = 'mongodb://localhost:27017/Birding';
+    // process.env.DB_URI = 'mongodb://localhost:27017/Birding';
+    process.env.DB_URI = 'mongodb+srv://kendsr:need2not@testcluster.fmbay.mongodb.net/Birding?retryWrites=true&w=majority';
 } 
